f49fb6a56bb3a4ab289adf7808cdd5be5b0231fa
ARUN KUMAR <ARUN KUMAR@Ganesha>
2024-07-07 19:31:16 +0530

    values modified

--- Hybrid_FrameWork/src/test/java/commonFunctions/Demo.java
+++ Hybrid_FrameWork/src/test/java/commonFunctions/Demo.java
@@ -3,29 +3,29 @@ package commonFunctions;
 public class Demo {
 
 	public void add() {
-		int a = 2;
-		int b = 3;
+		int a = 21;
+		int b = 13;
 		int c = a + b;
 		System.out.println(c);
 	}
 	
 	public void sub() {
-		int a = 12;
-		int b = 3;
+		int a = 121;
+		int b = 30;
 		int c = a - b;
 		System.out.println(c);
 	}
 	
 	public void div() {
-		int a = 12;
-		int b = 3;
+		int a = 11112;
+		int b = 30;
 		int c = a / b;
 		System.out.println(c);
 	}
 	
 	public void mult() {
-		int a = 12;
-		int b = 3;
+		int a = 412;
+		int b = 5;
 		int c = a * b;
 		System.out.println(c);
 	}
