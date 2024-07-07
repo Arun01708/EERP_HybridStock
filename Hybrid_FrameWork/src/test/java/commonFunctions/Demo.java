344a7755f8443ccbabd102f492f09bb5bd726a02
ARUN KUMAR <ARUN KUMAR@Ganesha>
2024-07-07 18:56:44 +0530

    New Class with name "Demo"

--- Hybrid_FrameWork/src/test/java/commonFunctions/Demo.java
+++ Hybrid_FrameWork/src/test/java/commonFunctions/Demo.java
@@ -2,4 +2,31 @@ package commonFunctions;
 
 public class Demo {
 
+	public void add() {
+		int a = 2;
+		int b = 3;
+		int c = a + b;
+		System.out.println(c);
+	}
+	
+	public void sub() {
+		int a = 12;
+		int b = 3;
+		int c = a - b;
+		System.out.println(c);
+	}
+	
+	public void div() {
+		int a = 12;
+		int b = 3;
+		int c = a / b;
+		System.out.println(c);
+	}
+	
+	public void mult() {
+		int a = 12;
+		int b = 3;
+		int c = a * b;
+		System.out.println(c);
+	}
 }
